58998937b83e6a0c4090a20a4015824a7cc45842
hyunseok <tjgustjr33@gmail.com>
2026-03-12 00:55:49 +0900

    🎉 Init Project!

---
+++ src/main/java/com/project/likelion14thbe/Likelion14thbeApplication.java
@@ -0,0 +1,13 @@
+package com.project.likelion14thbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Likelion14thbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Likelion14thbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/likelion14thbe/Likelion14thbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.likelion14thbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion14thbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
