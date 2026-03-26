615061267c8c39c738d627524f02150226db31f0
shlee72729799 <2tjdgns811@naver.com>
2026-03-26 19:55:43 +0900

    ✨feat: 1주차 실습

---
+++ src/main/java/com/project/likelion14thbe/domain/member/dto/reponse/MemberResDTO.java
@@ -0,0 +1,4 @@
+package com.project.likelion14thbe.domain.member.dto.reponse;
+
+public class MemberResDTO {
+}
---
+++ src/main/java/com/project/likelion14thbe/domain/member/entity/Member.java
@@ -0,0 +1,8 @@
+package com.project.likelion14thbe.domain.member.entity;
+
+
+//@Entity
+public class Member {
+
+
+}
