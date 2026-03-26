e3d98c71b009831ba14adf96750b5707aef28bcb
julia <julia6317@naver.com>
2026-03-26 19:57:37 +0900

    ? feat: 1주차 실습

---
+++ src/main/java/com/project/likelion14thbe/domain/member/repository/MemberRepository.java
@@ -0,0 +1,6 @@
+package com.project.likelion14thbe.domain.member.repository;
+
+import com.project.likelion14thbe.domain.member.entity.Member;
+
+public interface MemberRepository extends JpaRepository <Member, Long> {
+}
