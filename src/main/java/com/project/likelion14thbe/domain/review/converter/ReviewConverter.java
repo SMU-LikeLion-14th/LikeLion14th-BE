914572f3273a100ac8f5460eba6ea08fa938604a
julia <julia6317@naver.com>
2026-04-01 18:40:19 +0900

    ? feat: 1주차 실습(review)

---
+++ src/main/java/com/project/likelion14thbe/domain/review/converter/ReviewConverter.java
@@ -0,0 +1,9 @@
+package com.project.likelion14thbe.domain.review.converter;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+//생성자 접근 비허용
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ReviewConverter {
+}
---
+++ src/main/java/com/project/likelion14thbe/domain/review/entity/Review.java
@@ -0,0 +1,4 @@
+package com.project.likelion14thbe.domain.review.entity;
+
+public class Review {
+}
