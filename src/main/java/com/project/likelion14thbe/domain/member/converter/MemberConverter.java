b996d9a16f4e625cb042106a52ee898f43ef2648
kangho <kil0653@naver.com>
2026-04-02 20:29:18 +0900

    ✨ feat: member 패키지 생성

---
+++ src/main/java/com/project/likelion14thbe/domain/member/converter/MemberConverter.java
@@ -0,0 +1,8 @@
+package com.project.likelion14thbe.domain.member.converter;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MemberConverter {
+}
---
+++ src/main/java/com/project/likelion14thbe/domain/member/service/query/MemberQueryServicelmpl.java
@@ -0,0 +1,4 @@
+package com.project.likelion14thbe.domain.member.service.query;
+
+public class MemberQueryServicelmpl {
+}
