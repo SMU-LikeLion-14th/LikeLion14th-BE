5ee04b0c57b08ffd216b3567284ad23681903c08
sondg921 <sondg921@naver.com>
2026-04-02 20:22:27 +0900

    ✨feat: 1주차 Order 실습

--- src/main/java/com/project/likelion14thbe/domain/order/service/command/OrderCommandServiceImpl.java
+++ src/main/java/com/project/likelion14thbe/domain/order/service/command/OrderCommandServiceImpl.java
@@ -3,5 +3,5 @@ package com.project.likelion14thbe.domain.order.service.command;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrderCommandServiceImpl {
+public class OrderCommandServiceImpl implements OrderCommandService {
 }
